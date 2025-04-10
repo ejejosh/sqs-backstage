2b1ea07893456694578f39e85f200cd883e63486
Joshua Eje <eje_joshua@yahoo.com>
2025-04-10 15:15:48 +0100

    Updated IAM policy to add test to tiko_nudge

--- iam/policies.tf
+++ iam/policies.tf
@@ -5,7 +5,8 @@ data "aws_iam_policy_document" "tiko_nudge" {
     actions = local.publish_queue
 
     resources = [
-      data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-handle-inbound-message-command"],
+
+      data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-test"],      data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-handle-inbound-message-command"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-scheduler-commands"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-chatter-send-message"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-mobile-inbox-service"],
@@ -35,7 +36,8 @@ data "aws_iam_policy_document" "tiko_nudge" {
     actions = local.publish_topic
 
     resources = [
-      data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-tiko-miles-incentive-given"],
+
+      data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-test"],      data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-tiko-miles-incentive-given"],
       data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-reminder-sent"],
       data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-trial-started"],
       data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-trial-ended"],
