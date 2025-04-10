451ecd818b11d5b1a4db4879152e59f402096100
Joshua Eje <eje_joshua@yahoo.com>
2025-04-10 18:19:59 +0100

    Updated IAM policy to add jguoli to tiko_nudge

--- iam/policies.tf
+++ iam/policies.tf
@@ -5,6 +5,7 @@ data "aws_iam_policy_document" "tiko_nudge" {
     actions = local.publish_queue
 
     resources = [
+data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-jbui"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-handle-inbound-message-command"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-scheduler-commands"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-chatter-send-message"],
@@ -21,6 +22,7 @@ data "aws_iam_policy_document" "tiko_nudge" {
     actions = local.consume_queue
 
     resources = [
+data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-jbui"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-tiko-nudge-offer-validations"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-tiko-nudge-participants"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-tiko-nudge-scheduler-notifications"],
