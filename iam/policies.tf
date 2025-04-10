4762710ba35ef92adb48f6b0b2796c3d1c51ba79
Joshua Eje <eje_joshua@yahoo.com>
2025-04-10 13:43:51 +0100

    Updated IAM policy to add second-test to tiko_nudge_reminders

--- iam/policies.tf
+++ iam/policies.tf
@@ -65,6 +65,7 @@ data "aws_iam_policy_document" "tiko_nudge_reminders" {
     actions = local.publish_queue
 
     resources = [
+        data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-second-test"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-handle-inbound-message-command"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-scheduler-commands"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-chatter-send-message"],
@@ -81,6 +82,7 @@ data "aws_iam_policy_document" "tiko_nudge_reminders" {
     actions = local.consume_queue
 
     resources = [
+        data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-second-test"],
       data.terraform_remote_state.sqs_sns.outputs.queue_arns["${var.environment}-tiko-nudge-reminders-service"]
     ]
 
@@ -92,6 +94,7 @@ data "aws_iam_policy_document" "tiko_nudge_reminders" {
     actions = local.publish_topic
 
     resources = [
+        data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-second-test"],
       data.terraform_remote_state.sqs_sns.outputs.topic_arns["${var.environment}-reminder-sent"]
     ]
 
